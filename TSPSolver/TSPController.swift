5f4551ae49c46a553b527620d7f5878acb8cc01b
KazuyaMIURA <miup.0627@gmail.com>
2015-07-06 18:51:48 +0900

    add classes

---
+++ TSPSolver/TSPController.swift
@@ -0,0 +1,15 @@
+//
+//  TSPController.swift
+//  TSPSolver
+//
+//  Created by IllyasvielVonEinzbern on 7/6/15.
+//  Copyright (c) 2015 KazuyaMIURA. All rights reserved.
+//
+
+import UIKit
+
+class TSPController {
+    
+
+
+}
---
+++ TSPSolver/TSPVisualizer.swift
@@ -0,0 +1,20 @@
+//
+//  TSPVisualizer.swift
+//  TSPSolver
+//
+//  Created by IllyasvielVonEinzbern on 7/6/15.
+//  Copyright (c) 2015 KazuyaMIURA. All rights reserved.
+//
+
+import UIKit
+
+class TSPVisualizer {
+    let tspView: TSPView
+
+    init(view: TSPView) {
+        self.tspView = view
+    }
+
+    
+
+}
