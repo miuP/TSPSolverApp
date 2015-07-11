d682432d78508e99ecd00e1c13225d1d9e7b8ffb
KazuyaMIURA <miup.0627@gmail.com>
2015-07-11 10:45:30 +0900

    add class file

---
+++ TSPSolver/NearestNeighbor.swift
@@ -0,0 +1,12 @@
+//
+//  NearestNeighbor.swift
+//  TSPSolver
+//
+//  Created by IllyasvielVonEinzbern on 7/11/15.
+//  Copyright (c) 2015 KazuyaMIURA. All rights reserved.
+//
+
+import UIKit
+
+class NearestNeighbor: TSPSolver {
+}
